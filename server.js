38344c0b3ea3806125c843912f5fb6906343a25a
Reed Martin <reedhmartin@gmail.com>
2023-04-10 17:28:47 -0400

    Added TODOs to the server.js file.

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+// Import the required packages and files
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// TODO: Set up the Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// TODO: Set up the middleware to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// TODO: Set up the static folder for serving CSS and JS files
+app.use(express.static('public'));
+
+// TODO: Set up the API routes
+app.use('/api', apiRoutes);
+
+// TODO: Set up the HTML routes
+app.use('/', htmlRoutes);
+
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
