035b485979a339afcc9a5dc2bc9a4892985aab76
Reed Martin <reedhmartin@gmail.com>
2023-04-10 18:02:24 -0400

    Completed server.js file.

--- server.js
+++ server.js
@@ -3,21 +3,21 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-// TODO: Set up the Express app
+// Set up the Express app
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// TODO: Set up the middleware to handle data parsing
+// Set up the middleware to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// TODO: Set up the static folder for serving CSS and JS files
+// Set up the static folder for serving CSS and JS files
 app.use(express.static('public'));
 
-// TODO: Set up the API routes
+// Set up the API routes
 app.use('/api', apiRoutes);
 
-// TODO: Set up the HTML routes
+// Set up the HTML routes
 app.use('/', htmlRoutes);
 
 // Start the server and listen on the specified port
